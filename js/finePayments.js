686e998d8ea06804c307bf75351b8ebae433bec9
Ivanna Svynar <svynarivanna@gmail.com>
2024-02-23 18:02:56 +0200

    Fine_payment_decision

--- js/finePayments.js
+++ js/finePayments.js
@@ -15,24 +15,53 @@ let buttonSubmit = document.getElementById("payFine");
 let DB = data.finesData;
 
 
-/**
-Вам необхідно реалізувати наступний функціонал.
-Зробити валідацію до всіх полів
-1. Номер та сума повинні бути однакові як в існуючого штрафу - якщо ні видавати
-alert "Номер не співпадає" або "Сума не співпадає"
-
-2. Паспортні дані у форматі - перші дві літери укр алфавіту, та 6 цифр.
-Якщо не співпадає то видавати alert "Не вірний паспортний номер"
-
-3. Номер кредитної карки 16 цифр -
-якщо не співпадає то видавати alert "Не вірна кредитна картка"
-
-4. cvv 3 цифри - якщо не співпадає то видавати alert "Не вірний cvv".
-
-Якщо валідація проходить успішно, то виконати оплату,
- тобто вам потрібно видалити обєкт з DB
- */
-buttonSubmit.addEventListener('click',payFine);
-function payFine(){
+function validateFields() {
+let fineExists = false;
+for (let i = 0; i < DB.length; i++) {
+    if (DB[i].номер === fineNumber.value) {
+        fineExists = true;
+        if (parseFloat(amount.value) !== DB[i].сума) {
+            alert("Сума не співпадає");
+            return false;
+        }
+        break;
+    }   
+}
+if (!fineExists) {
+    alert("Номер не співпадає");
+    return false;
+}
+let validatePassport = /^[А-ЩЬЮ-ЯҐЄІЇ]{2}\d{6}$/;
+if (!validatePassport.test(passport.value)) {
+    alert("Не вірний паспортний номер");
+        return false;
+}
+let validateCreditCard = /^\d{16}$/;
+if (!validateCreditCard.test(creditCardNumber.value)) {
+    alert("Не вірна кредитна картка");
+        return false;
+}
+let validateCVV = /^\d{3}$/;
+if (!validateCVV.test(cvv.value)) {
+    alert("Не вірний cvv");
+    return false;
+}
+return true;
+}
+function payFine() {
+    for (let i = 0; i < DB.length; i++) {
+        if (DB[i].номер === fineNumber.value) {
+            DB.splice(i, 1);
+            break;
+        }
+    }
 
 }
+
+buttonSubmit.addEventListener('click',function() {
+if (validateFields()) {
+    payFine();
+    alert("Штраф оплачено!")
+}
+});
+
